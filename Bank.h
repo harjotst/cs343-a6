c163319157fc23d08453a54f3e6ae9bde761451b
jessicabohm <j2bohm@uwaterloo.ca>
2023-11-30 19:44:24 -0500

    separated files

---
+++ Bank.h
@@ -0,0 +1,39 @@
+// manages all the students account info
+_Monitor Bank {
+	unsigned int *student_accounts;
+	uCondition *account_locks;
+
+  public:
+	Bank( unsigned int numStudents ) {
+		student_accounts = new int[numStudents]; // maybe use uNoCtor - apparently C++ guarentees mem is 0 initialized does uC++?
+		account_locks = new uCondition[numStudents];
+
+		// all students start with $0
+		for(int i = 0; i < numStudent; i++) {
+			student_accounts[i] = 0;
+		}
+	}
+
+	// destructor called by main at the end of execution
+	~Bank() {
+		delete [] student_accounts;
+		delete [] account_locks;
+	}
+
+	void deposit( unsigned int id, unsigned int amount ) {
+		student_accounts[id] += amount;
+		account_locks[id].signal();
+	}
+
+	void withdraw( unsigned int id, unsigned int amount ) {
+		// if the students account doesn't have enough money to withdraw amount, wait until it does
+		// this can take more than one deposit
+		while(student_accounts[id] < amount) {
+			// wait on that specific deposit into this students account
+			account_locks[id].wait();
+		}
+
+		student_accounts[id] -= amount;
+	}
+
+};
--- soda.h
+++ soda.h
@@ -1,4 +1,4 @@
-#include <uSemaphore.h>
+
 
 _Task Student {
 	Printer * prt;
@@ -45,124 +45,11 @@ class WATCard {
 
 
 
-// manages all the students account info
-_Monitor Bank {
-	unsigned int *student_accounts;
-	uCondition *account_locks;
-
-  public:
-	Bank( unsigned int numStudents ) {
-		student_accounts = new int[numStudents]; // maybe use uNoCtor - apparently C++ guarentees mem is 0 initialized does uC++?
-		account_locks = new uCondition[numStudents];
-
-		// all students start with $0
-		for(int i = 0; i < numStudent; i++) {
-			student_accounts[i] = 0;
-		}
-	}
-
-	// destructor called by main at the end of execution
-	~Bank() {
-		delete [] student_accounts;
-		delete [] account_locks;
-	}
-
-	void deposit( unsigned int id, unsigned int amount ) {
-		student_accounts[id] += amount;
-		account_locks[id].signal();
-	}
-
-	void withdraw( unsigned int id, unsigned int amount ) {
-		// if the students account doesn't have enough money to withdraw amount, wait until it does
-		// this can take more than one deposit
-		while(student_accounts[id] < amount) {
-			// wait on that specific deposit into this students account
-			account_locks[id].wait();
-		}
-
-		student_accounts[id] -= amount;
-	}
-
-};
-
-_Task Parent {
-	Printer * prt;
-	Bank * bank;
-	unsigned int numStudents;
-	unsigned int parentalDelay;
-
-		
-	void main() {
-		// distribute money to students until destructor is called
-		for(;;) {
-			// attempt the except of the Parent destructor without the acceptor blocking
-			// this causes parent to perform a yielding busy-wait on calls to its destructor
-			_Accept(~Parent) {
-				break;
-
-			// if no call to destructor continue distributing money
-			} _Else {}
-
-			yield(parentalDelay);
-			unsigned int sid = prng(0, numStudents - 1); // prng(l, u) [l,u]
-			unsigned int amount = prng(1, 3);
-
-			bank->deposit(id, amount);
-		}
-	}
-
-  public:
-	Parent( Printer & prt, Bank & bank, unsigned int numStudents, unsigned int parentalDelay ) : prt(prt), bank(bank), numStudents(numStudents), parentalDelay(parentalDelay) {}
-};
-
-_Task Groupoff {
-	Printer * prt;
-	unsigned int numStudents;
-	unsigned int sodaCost;
-	unsigned int groupoffDelay;
-	WATCard* WATCards;
-	unsigned int add_WATCard_index_count;
-	uLock lock;
-
-
-	void main() {
-		// wait until all student request a gift-card to start distributing them
-		for(;;) {
-			// attempt the except of the Groupoff destructor without the acceptor blocking
-			// this causes groupoff to perform a yielding busy-wait on calls to its destructor
-			_Accept(~groupoff) {
-				break;
-
-			// if no call to destructor continue distributing giftcards
-			// note: _Else is not busy waiting because there are a finite number of students.
-			} _Else {}
 
 
 
-		}
-	}
 
-  public:
-	Groupoff( Printer & prt, unsigned int numStudents, unsigned int sodaCost, unsigned int groupoffDelay ) : prt(prt), numStudents(numStudents), sodaCost(sodaCost), groupoffDelay(groupoffDelay) {
-		WATCards = new WATCard[numStudents];
-		add_WATCard_index_count = 0;
-	}
-	
-	// called by all student
-	WATCard::FWATCard giftCard() {
-		// no barging prevention, but doesn't matter here just need to set up an array of WATCards in arbitrary order
-		lock.acquire();
-		unsigned int IDX = add_WATCard_idx;
-		add_WATCard_index_count += 1;
-		lock.release();
 
-		WATCard* wc = new WATCard();
-		wc->deposit(sodaCost);
-		WATCards[add_WATCard_idx] = wc;
-		
-		return wc->
-	}
-};
 
 _Task VendingMachine {
 	void main();
