a7d752b5c7d8f334425490301f02d7e8abb93e52
Jessica Bohm <j2bohm@uwaterloo.ca>
2023-12-03 23:18:17 -0500

    updated WATCardOffice to run without errors

--- WATCardOffice.h
+++ WATCardOffice.h
@@ -42,48 +42,48 @@ _Task WATCardOffice {
 	Bank * bank;
 	unsigned int numCouriers;
 
-	deque<Job*> job_buf;
+	int closePool;
 
-	_Event ClosePool {};
+	deque<Job*> job_buf;
 
 	_Task Courier {
 		WATCardOffice *office;
 
 		void main() {
-			// handle the job requests in the order they come in
 			for(;;) {
-
-				// blocks until a job request is ready
 				Job *job = office->requestWork();
 
-				if (job == nullptr) {
+				// nullptr signals end of jobs - triggered by WATCard office destruction
+				// break to terminate main
+				if(job == nullptr) {
 				 	break;
 				}
-
 
 				// handle job request - communication with the bank
                 // blocks until enough funds in students account to withdraw
 				office->bank->withdraw(job->args.sid, job->args.amount);
 
 
-				// update the students WATCard
-                // check if WATCard is lost (1/6 chance)
+				// update the students WATCard:
+                
+				// check if WATCard is lost (1/6 chance)
 				unsigned int lost_WATCard = prng(6);
 				if(lost_WATCard == 0) {
-					// set the futures exception and set the fwc pointer to wc to singal its update
-					WATCardOffice::Lost *exception = new WATCardOffice::Lost();
-					job->result.exception(exception);
-                    job->result.delivery(job->args.card);
-				
+					// signal a Lost exception 
+					job->result.exception(new WATCardOffice::Lost());
+
+					// delete the WATCard since the student cannot
+					delete job->args.card;
+
 				} else {
 					// deposit the funds into the WATCard
-                    // might block if another task is accessing the WATCard
 					job->args.card->deposit(job->args.amount);
 
-					job->result.delivery(job->args.card); // signal the WATCard is ready by setting the future
+					// signal the WATCard is ready by setting the future
+					job->result.delivery(job->args.card); 
 				}
 
-				// delete the Job since it was declared on heap (ensure it isn't deleting the WATCard)
+				// delete the Job since it was declared on heap
 				delete job;
 			}
 		}
@@ -91,7 +91,7 @@ _Task WATCardOffice {
 	  public:
 		Courier(WATCardOffice * office) : office(office) {};
 
-	}; // communicates with bank
+	};
 
 	vector<Courier*> couriers;
 
@@ -99,10 +99,10 @@ _Task WATCardOffice {
   public:
   
 	_Event Lost {}; // lost WATCard
-	WATCardOffice( Printer & prt, Bank & bank, unsigned int numCouriers ) : prt(&prt), bank(&bank), numCouriers(numCouriers) {
-	}
+	WATCardOffice( Printer & prt, Bank & bank, unsigned int numCouriers ) : prt(&prt), bank(&bank), numCouriers(numCouriers), closePool(0) {}
 
-	
+	// called by student to create a new WATCard
+	// return a future WATCard	
 	WATCard::FWATCard create( unsigned int sid, unsigned int amount ) __attribute__(( warn_unused_result )) {
 		// create a 'real' WATCard with an inital balance
 		// put on the heap so it persists after the return
@@ -117,7 +117,7 @@ _Task WATCardOffice {
 	}
 
 	// called by student when their card indicates insufficent funds
-	// return a future WATCard - the student can then call select on this ??
+	// return a future WATCard
 	WATCard::FWATCard transfer( unsigned int sid, unsigned int amount, WATCard * card ) __attribute__(( warn_unused_result )) {
 		WATCard::FWATCard fwc;
         
@@ -130,16 +130,13 @@ _Task WATCardOffice {
 
 	// blocks until a Job request is ready, and receives next request as result of call - money sent from parent
 	Job * requestWork() __attribute__(( warn_unused_result )) {
-		// try to get a job from the buffer
-		if(job_buf.empty()) {
-			try {
-				_Accept(create || transfer);
-
-			} _Catch(uMutexFailure::RendezvousFailure &e) {
-				return nullptr;
-			} 
+		// if the pool closed flag has been set after the WATCardOffice destructor is called
+		// return nullptr to signal the Courier task to terminate
+		if(closePool == 1) {
+			return nullptr;
 		}
 		
+		// otherwise return the first job in queue (FIFO) and remove from the buffer
 		Job *job = job_buf.front();
 		job_buf.pop_front();
 		
@@ -149,20 +146,32 @@ _Task WATCardOffice {
   private:
 
 	void main() {
-		// create a courier pool of numCouriers
-		
-		// pass each courier task the watcard office
+		// create a courier pool of numCouriers and pass each courier task a pointer to WATCardOffice
 		for(int i = 0; i < numCouriers; i++) {
 			couriers.push_back(new Courier(this));
 		}
 
-		_Accept(create || transfer || requestWork) {}
-		or _Accept(~WATCardOffice) {
-			cout << "start courier deletion" << endl;
-			// only called when all job requests have been handled -> buffer is empty
-			for(int i = 0; i < numCouriers; i++) {
-				_Resume ClosePool() _At *couriers[i];
-				delete couriers[i];
+		for(;;) {
+			_When(!job_buf.empty()) _Accept(requestWork) {}
+			or _Accept(create || transfer) {}
+			or _Accept(~WATCardOffice) {
+				// only called when all job requests have been handled -> buffer is empty
+				// all numCouriers tasks will be waiting on requestWork
+
+				closePool = 1;
+
+				// resume each courier task waiting on requestWork
+				// they will see the pool has closed and terminate main
+				for(int i = 0; i < numCouriers; i++) {
+					_Accept(requestWork);
+				}
+
+				// delete all the courier tasks now that they have finished execution
+				for(int i = 0; i < numCouriers; i++) {
+					delete couriers[i];
+				}
+
+				break;
 			}
 		}
 	}
